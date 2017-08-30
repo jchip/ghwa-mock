9e8fa74b4cb60e3905d37f77e40be2507a151885
Joel Chen <joel123@gmail.com>
2017-08-30 14:52:51 -0700

    filter order within 6 months only

--- make-orders-list.js
+++ make-orders-list.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const getOrders = require("./get-orders");
+const MONTH_MILLISEC = 6 * 24 * 60 * 60 * 1000 * 30;
 
 function makeOrdersList() {
   return getOrders()
@@ -9,14 +10,20 @@ function makeOrdersList() {
       const summary = payload && payload.orderSummary;
       if (!summary) return {};
       let n = 0;
+      const now = new Date();
       const msg = summary
+        .filter(s => {
+          const od = new Date(s.orderDate);
+          const diff = now - od;
+          return diff < MONTH_MILLISEC;
+        })
         .map(s => {
           const lines = s.orderLines;
           n++;
           return `Order number ${n} with item ${lines[0].orderProduct.productName}`;
         })
         .join(". ");
-      const text = `You have ${summary.length} recent orders. ${msg}`;
+      const text = `You have ${n} recent orders. ${msg}`;
       return {
         speech: text,
         displayText: text
